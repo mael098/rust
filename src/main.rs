c97c92e952ebc8693ffb864a10177520535965e2
mael098 <juanvillegasmorales@outlook.com>
2023-08-26 22:02:37 -0600

    aprendiendo rust

---
+++ src/main.rs
@@ -0,0 +1,13 @@
+use std::io;
+
+fn main() {
+    print!("Hello, world!");
+    print!("I'm a Rustacean!"); // no newline is printed!
+    let mut guess = String::new();
+    io::stdin()
+    .read_line(&mut guess)
+    .expect("Failed to read line");
+
+}
+
+
